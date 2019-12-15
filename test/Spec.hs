00e2b23710a43a4fffdb63f775269a284ea573c5
Nikolas <nikmaehl@uni-bremen.de>
2019-12-15 21:09:23 +0100

    more tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -62,6 +62,9 @@ main = hspec $ do
             whitespaceIndependent ["x < y", "||", "y < z"] $ 
                 \s -> parseBExp s `shouldBe` (Right (BOr (BLess x y) (BLess y z)))
 
+        it "parses parentheses" $ do
+            parseBExp "(x < y) || ((y < z) && true)" `shouldBe` (Right (BOr (BLess x y) (BAnd (BLess y z) BTrue)))
+
     describe "Parser.Statement" $ do
         let parseStmt t = parse statement "" t
         let x = Idt "x"
@@ -87,21 +90,27 @@ main = hspec $ do
             parseAExp "-1337" `shouldBe` (Right (ALit (-1337)))
 
         it "parses arithmetic operators" $ do
-            parseAExp "1 + 2;" `shouldBe` (Right (ABinExp Add (ALit 1) (ALit 2)))
-            parseAExp "x - y;" `shouldBe` (Right (ABinExp Sub x y))
-            parseAExp "x * y;" `shouldBe` (Right (ABinExp Mul x y))
-            parseAExp "x / y;" `shouldBe` (Right (ABinExp Div x y))
+            parseAExp "1 + 2" `shouldBe` (Right (ABinExp Add (ALit 1) (ALit 2)))
+            parseAExp "x - y" `shouldBe` (Right (ABinExp Sub x y))
+            parseAExp "x * y" `shouldBe` (Right (ABinExp Mul x y))
+            parseAExp "x / y" `shouldBe` (Right (ABinExp Div x y))
 
         it "ignores whitespaces" $ do
             whitespaceIndependent ["x", "+", "y"] $ 
                 \s -> parseAExp s  `shouldBe` (Right (ABinExp Add x y))
 
         it "parses addition left associative" $ do
-            parseAExp "x + y + z;" `shouldBe` (Right (ABinExp Add (ABinExp Add x y) z))
+            parseAExp "x + y + z" `shouldBe` (Right (ABinExp Add (ABinExp Add x y) z))
 
         it "parses multiplication left associative" $ do
-            parseAExp "x * y * z;" `shouldBe` (Right (ABinExp Mul (ABinExp Mul x y) z))
+            parseAExp "x * y * z" `shouldBe` (Right (ABinExp Mul (ABinExp Mul x y) z))
 
         it "parses multiplication with higher precedence than addition" $ do
-            parseAExp "x + y * z;" `shouldBe` (Right (ABinExp Add  x (ABinExp Mul y z)))
+            parseAExp "x + y * z" `shouldBe` (Right (ABinExp Add  x (ABinExp Mul y z)))
+
+        it "parses nested parentheses" $ do
+            parseAExp "x - (3 / (y + z))" `shouldBe` (Right (ABinExp Sub  x (ABinExp Div (ALit 3) (ABinExp Add y z))))
+
+        it "parses parentheses with higher precedence than multiplication" $ do
+            parseAExp "x * (y - z)" `shouldBe` (Right (ABinExp Mul  x (ABinExp Sub y z)))
 
